f89c73a0541126f472af38fa7dad475551d7ede6
nookworth <nooksack@gmail.com>
2025-01-02 15:42:54 -0700

    added colors to theme + use conditional server url

--- src/hooks/useFetchPRs.ts
+++ src/hooks/useFetchPRs.ts
@@ -1,11 +1,17 @@
 import { useEffect, useState } from 'react';
 
+const env = process.env.NODE_ENV;
+const url =
+  env === 'development'
+    ? 'http://localhost:3000'
+    : 'https://tpg-dev-portal-server.fly.dev';
+
 export const useFetchPRs = () => {
   const [fetchedPRs, setFetchedPRs] = useState<any[]>([]);
   const [error, setError] = useState<string | null>(null);
 
   useEffect(() => {
-    fetch('https://tpg-dev-portal-server.fly.dev')
+    fetch(url)
       .then((response) => {
         response
           .json()
